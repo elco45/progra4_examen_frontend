4a7d29cd111804d26f4580479d52f5f6d5b310d8
elco45 <dennischeong2003@yahoo.es>
2015-12-14 10:14:52 -0600

    url

--- app/scripts/services/HomeService.js
+++ app/scripts/services/HomeService.js
@@ -1,7 +1,7 @@
 angular.module('AngularScaffold.Services').factory('HomeService', ['$http',
 	function($http){
 		$http.defaults.withCredentials = true;
-		var baseUrl = 'http://localhost:8000/';
+		var baseUrl = 'https://dennis-cheong-backend.herokuapp.com/';
 		return {
 			Login: function(payload){
 				return $http.post(baseUrl + "v1/login", payload);
